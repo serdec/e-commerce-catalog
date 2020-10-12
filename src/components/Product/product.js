a5a434e0db296c1585d4d5a2f3bb05457b2fd95a
Sergio de Cristofaro <sdecri@gmail.com>
2020-10-12 15:45:23 +0200

    added remove-from-cart feature

--- src/components/Product/product.js
+++ src/components/Product/product.js
@@ -4,14 +4,14 @@ import PropTypes from 'prop-types';
 import Product__ImageWrapper from './__ImageWrapper/product__image-wrapper';
 import Product__Details from './__Details/product__details';
 import { isProductInBag } from '../Bag/reducer';
-import { addToBag } from '../Bag/reducer';
+import { addToBag, removeFromBag } from '../Bag/reducer';
 import {
   addToWishlist,
   isProductInWishlist,
   removeFromWishlist,
 } from '../Wishlist/reducer';
 
-const noop = () => {};
+const noop = () => { };
 
 const mapStateToProps = (state) => ({
   isProductInBag: isProductInBag(state.bag),
@@ -19,6 +19,7 @@ const mapStateToProps = (state) => ({
 });
 const mapDispatchToProps = {
   addToBag,
+  removeFromBag,
   addToWishlist,
   removeFromWishlist,
 };
@@ -27,14 +28,21 @@ export const Product = ({
   product = {},
   isProductInBag = noop,
   addToBag = noop,
+  removeFromBag = noop,
   isProductInWishlist = noop,
   addToWishlist = noop,
   removeFromWishlist = noop,
 } = {}) => {
+  const inBag = isProductInBag(product.uuid);
+  const onBagClick = inBag
+    ? () => removeFromBag(product.uuid)
+    : () => addToBag(product);
+
   const inWishlist = isProductInWishlist(product.uuid);
   const onWishlistClick = inWishlist
     ? () => removeFromWishlist(product.uuid)
     : () => addToWishlist(product);
+
   return (
     <article className="product" itemScope itemType="http://schema.org/Product">
       <Product__ImageWrapper
@@ -47,7 +55,7 @@ export const Product = ({
         retail_price={product.retail_price?.formatted_value}
         net_price={product.net_price?.formatted_value}
         inBag={isProductInBag(product.uuid)}
-        addToBag={() => addToBag(product)}
+        onBagClick={() => onBagClick(product)}
       />
     </article>
   );
@@ -57,6 +65,7 @@ Product.propTypes = {
   product: PropTypes.object,
   isProductInBag: PropTypes.func,
   addToBag: PropTypes.func,
+  removeFromBag: PropTypes.func,
   isProductInWishlist: PropTypes.func,
   addToWishlist: PropTypes.func,
   removeFromWishlist: PropTypes.func,
