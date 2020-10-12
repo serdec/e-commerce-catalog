dc3b01c168ffac6541502edbbe247e61a6df7987
Sergio de Cristofaro <sdecri@gmail.com>
2020-10-12 11:25:45 +0200

    added add/remove from wishlist feature

--- src/components/Product/__ImageWrapper/product__image-wrapper.js
+++ src/components/Product/__ImageWrapper/product__image-wrapper.js
@@ -3,14 +3,24 @@ import PropTypes from 'prop-types';
 import Product__Image from '../__Image/product__image';
 import Product__WishlistButton from '../__wishlistButton/product__wishlist-button';
 
-const Product__ImageWrapper = ({ cover_image_url = '' } = {}) => (
+const noop = () => {};
+const Product__ImageWrapper = ({
+  cover_image_url = '',
+  inWishlist = false,
+  onWishlistClick = noop,
+} = {}) => (
   <figure className="product__image-wrapper">
     <Product__Image img={cover_image_url} />
-    <Product__WishlistButton />
+    <Product__WishlistButton
+      inWishlist={inWishlist}
+      onWishlistClick={onWishlistClick}
+    />
   </figure>
 );
 
 Product__ImageWrapper.propTypes = {
   cover_image_url: PropTypes.string,
+  inWishlist: PropTypes.bool,
+  onWishlistClick: PropTypes.func,
 };
 export default Product__ImageWrapper;
--- src/components/Product/__wishlistButton/product__wishlist-button-test.js
+++ src/components/Product/__wishlistButton/product__wishlist-button-test.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import { describe } from 'riteway';
+import match from 'riteway/match';
 import render from 'riteway/render-component';
-import Product__WishlistButton from './product__wishlist-button';
+import { Product__WishlistButton } from './product__wishlist-button';
 
 describe('product__wishilist-button', async (assert) => {
-  const createProduct__WishlistButton = () =>
-    render(<Product__WishlistButton />);
+  const createProduct__WishlistButton = ({ inWishlist = false } = {}) =>
+    render(<Product__WishlistButton inWishlist={inWishlist} />);
 
   {
     const $ = createProduct__WishlistButton();
@@ -14,6 +15,28 @@ describe('product__wishilist-button', async (assert) => {
       should: 'render the product wishlist button',
       expected: 1,
       actual: $('.product__wishlist-button').length,
+    });
+  }
+  {
+    const $ = createProduct__WishlistButton({ inWishlist: false });
+    const contains = match($.html());
+    const textToSearch = 'button--wishlist';
+    assert({
+      given: 'a product not in the wishlist',
+      should: 'render the button with the matching class',
+      expected: textToSearch,
+      actual: contains(textToSearch),
+    });
+  }
+  {
+    const $ = createProduct__WishlistButton({ inWishlist: true });
+    const contains = match($.html());
+    const textToSearch = 'button--inWishlist';
+    assert({
+      given: 'a product in the wishlist',
+      should: 'render the button with the class inWishilist',
+      expected: textToSearch,
+      actual: contains(textToSearch),
     });
   }
 });
